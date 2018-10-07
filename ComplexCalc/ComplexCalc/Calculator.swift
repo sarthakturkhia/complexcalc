e6934a530a94a3a6310636151e716a1bf6f5f8d0
Sarthak Turkhia <sarthakturkhia@Sarthaks-MacBook-Pro.local>
2018-10-06 19:25:15 -0700

    added source files

---
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -0,0 +1,20 @@
+//
+//  Calculator.swift
+//  ComplexCalc
+//
+//  Created by Ted Neward on 10/4/17.
+//  Copyright © 2017 Neward & Associates. All rights reserved.
+//
+
+import Foundation
+
+// All your work will go in here
+class Calculator {
+    func add(lhs:Int, rhs:Int) -> Int {
+        return lhs + rhs
+    }
+    func <#name#>(<#parameters#>) -> <#return type#> {
+        <#function body#>
+    }
+    
+}

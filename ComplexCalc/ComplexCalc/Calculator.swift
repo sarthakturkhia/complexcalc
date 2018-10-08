9c9ae39c5fc9df6108bae7c1816b915fc463132d
Sarthak Turkhia <sarthakturkhia@Sarthaks-MacBook-Pro.local>
2018-10-08 16:29:17 -0700

    MathOp function remaining

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -13,8 +13,60 @@ class Calculator {
     func add(lhs:Int, rhs:Int) -> Int {
         return lhs + rhs
     }
-    func <#name#>(<#parameters#>) -> <#return type#> {
-        <#function body#>
+    func subtract(lhs:Int, rhs:Int) -> Int {
+        return lhs - rhs
     }
+    func multiply(lhs:Int, rhs:Int) -> Int {
+        return lhs * rhs
+    }
+    func divide(lhs:Int, rhs:Int) -> Int {
+        return lhs / rhs
+    }
+    func add(_ arr:[Int]) -> Int {
+        return arr.reduce(0, { x, y in
+            x + y
+        })}
+    func subtract(_ arr:[Int]) -> Int {
+        return arr.reduce(0, { x, y in
+            x - y
+        })}
+    func multiply(_ arr:[Int]) -> Int {
+        return arr.reduce(0, { x, y in
+            x * y
+        })}
+    func divide(_ arr:[Int]) -> Int {
+        return arr.reduce(0, { x, y in
+            x / y
+        })}
+    
+    func count(_ arr:[Int]) -> Int {
+        return arr.count
+    }
+    
+    func avg(_ arr:[Int]) -> Int {
+        if count(arr) == 0 {
+        return 0
+        }
+        return (add(arr)/count(arr))
+    }
+    
+    func add(lhs:(Int, Int), rhs:(Int, Int)) -> (Int, Int) {
+        return (lhs.0+rhs.0, lhs.1+rhs.1)
+    }
+    
+    func subtract(lhs:(Int, Int), rhs:(Int, Int)) -> (Int, Int) {
+        return (lhs.0-rhs.0, lhs.1-rhs.1)
+    }
+    
+    func add(lhs:[String:Int], rhs:[String:Int]) -> [String:Int] {
+        return ["x": lhs["x"]! + rhs["x"]!, "y": lhs["y"]! + rhs["y"]!]
+    }
+    
+    func subtract(lhs:[String:Int], rhs:[String:Int]) -> [String:Int] {
+        return ["x": lhs["x"]! - rhs["x"]!, "y": lhs["y"]! - rhs["y"]!]
+    }
+    
+    
+  
     
 }
